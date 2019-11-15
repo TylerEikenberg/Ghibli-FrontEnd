723fc5f773d9e477e45b9a7b12215f7b1ba410ab
Tyler Eikenberg <tylereikenberg8195@gmail.com>
2019-11-15 11:36:39 -0500

    creating listOfFilms from database

--- src/App.js
+++ src/App.js
@@ -17,10 +17,37 @@ class App extends Component {
   }
 
   componentDidMount() {
-    axios.get(filmsUrl).then(res => {
-      let filmsUrlJson = JSON.stringify(res);
-      console.log(filmsUrlJson);
-    });
+    const getAllData = async () => {
+      await axios.get(filmsUrl).then(res => {
+        // let filmsUrlJson = JSON.stringify(res);
+        let filmsUrlData = res;
+        const ghibliFilms = filmsUrlData.data.map(item => {
+          const film = {};
+          film.id = item.id;
+          film.description = item.description;
+          film.director = item.director;
+          film.title = item.title;
+          film.producer = item.producer;
+          film.release_date = item.release_date;
+          film.url = item.url;
+          film.locations = item.locations;
+          film.people = item.people;
+          return film;
+        });
+        this.setState({ listOfFilms: ghibliFilms });
+        console.log(this.state.listOfFilms);
+      });
+      await axios.get(peopleUrl).then(res => {
+        let peopleUrlJson = JSON.stringify(res);
+        console.log("got people");
+      });
+      await axios.get(locationsUrl).then(res => {
+        let locationUrlJson = JSON.stringify(res);
+        console.log("got locations");
+      });
+    };
+
+    getAllData().then(() => {});
   }
   render() {
     return (
