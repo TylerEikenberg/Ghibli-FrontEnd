c08c9bed2710b7db73b5f50c88fdaed1d964ea01
Tyler Eikenberg <tylereikenberg8195@gmail.com>
2019-11-15 13:35:38 -0500

    added hovers to List component

--- src/Components/List.js
+++ src/Components/List.js
@@ -4,12 +4,34 @@ import "./List.css";
 class List extends Component {
   constructor(props) {
     super(props);
-    // console.log(this.props);
+    this.state = {
+      isHover: false
+    };
+
+    this.isMouseHover = this.isMouseHover.bind(this);
   }
 
+  isMouseHover = () => {
+    if (!this.state.isHover) {
+      this.setState({ isHover: true });
+    }
+    if (this.state.isHover) {
+      this.setState({ isHover: false });
+    }
+  };
+
   render() {
+    const activeClass = this.state.isHover
+      ? "list-group-item active"
+      : "list-group-item";
     return (
-      <li className="list-group-item">{this.props.name || this.props.title}</li>
+      <li
+        className={activeClass}
+        onMouseEnter={this.isMouseHover}
+        onMouseLeave={this.isMouseHover}
+      >
+        {this.props.name || this.props.title}
+      </li>
     );
   }
 }
