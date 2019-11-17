ea1c6c82cd28082b7f169b1f75f2484b2c23cb3d
Tyler Eikenberg <tylereikenberg8195@gmail.com>
2019-11-16 19:04:57 -0500

    added character create form

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from "react";
 import "./App.css";
 import List from "./Components/List";
+
 import InfoBox from "./Components/InfoBox/InfoBox";
 import Toolbar from "./Components/Toolbar/Toolbar";
 import SideDrawer from "./Components/SideDrawer/SideDrawer";
 import Overlay from "./Components/Overlay/Overlay";
+import CreateBox from "./Components/CreateBox/CreateBox";
 const axios = require("axios");
 
 const filmsUrl = "http://localhost:4000/films";
@@ -19,9 +21,9 @@ class App extends Component {
       listOfPeople: [],
       listofLocations: [],
       sideDrawerOpen: false,
-      filmsOn: false,
+      filmsOn: true,
       peopleOn: false,
-      locationsOn: true,
+      locationsOn: false,
       loading: true
     };
   }
@@ -114,8 +116,6 @@ class App extends Component {
    * LOOP THROUGH FILMS AND CREATE LIST ITEM OF EACH ONE
    */
   render() {
-    console.log(this.state.listofLocations);
-    console.log(this.state.listOfFilms);
     let overlay;
     let infoBox;
 
@@ -141,10 +141,26 @@ class App extends Component {
             {overlay}
           </div>
 
-          <div className="main-window">{infoBox}</div>
+          <div className="main-window">
+            {infoBox}
+            <CreateBox />
+          </div>
 
           <footer className="app-footer">
-            <h1 className="app-text">GHIBLI API FOOTER</h1>
+            <a
+              href="https://github.com/TylerEikenberg"
+              target="_blank"
+              className="app-text"
+            >
+              Made by Tyler Eikenberg
+            </a>
+            <a
+              href="https://ghibli-api-tse.herokuapp.com/"
+              target="_blank"
+              className="app-text"
+            >
+              Powered by Studio Ghibli API
+            </a>
           </footer>
         </div>
       );
---
+++ src/Components/CreateBox/CreateBox.js
@@ -0,0 +1,60 @@
+import React, { Component } from "react";
+import axios from "axios";
+
+class CreateBox extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      name: "",
+      gender: ""
+    };
+  }
+
+  changeHandler = event => {
+    this.setState({ [event.target.name]: event.target.value });
+  };
+
+  submitHandler = event => {
+    event.preventDefault();
+    console.log(this.state);
+    axios
+      .post("http://localhost:4000/people/create", this.state)
+      .then(response => {
+        console.log(response);
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  };
+
+  render() {
+    const { name, gender } = this.state;
+    return (
+      <div className="create-box-container">
+        <h2>Create a character</h2>
+        <form onSubmit={this.submitHandler}>
+          <div>
+            <input
+              type="text"
+              name="name"
+              value={name}
+              onChange={this.changeHandler}
+            />
+          </div>
+          <div>
+            <input
+              type="text"
+              name="gender"
+              value={gender}
+              onChange={this.changeHandler}
+            />
+          </div>
+          <button type="submit">Submit</button>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default CreateBox;
