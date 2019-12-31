b608073bc0f20a5700b9de2162236991094bacbd
Tyler Eikenberg <tylereikenberg8195@gmail.com>
2019-12-31 14:07:10 -0500

    converted createbox to functional components using hooks

--- src/Components/CreateBox/CreateBox.js
+++ src/Components/CreateBox/CreateBox.js
@@ -1,67 +1,75 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import axios from "axios";
 import "./CreateBox.css";
 
-class CreateBox extends Component {
-  constructor(props) {
-    super(props);
+/**
+ * recreate createBox for state to be handled by hooks
+ */
 
-    this.state = {
-      name: "",
-      gender: ""
-    };
-  }
+function CreateBox(props) {
+  // this.state = {
+  //   name: "",
+  //   gender: ""
+  // };
 
-  changeHandler = event => {
-    this.setState({ [event.target.name]: event.target.value });
+  const [name, setName] = useState("");
+  const [gender, setGender] = useState("");
+
+  const changeHandler = event => {
+    if (event.target.name === "gender") {
+      console.log(event.target.value);
+      setGender({ [event.target.name]: event.target.value });
+    }
+    if (event.target.name === "name") {
+      console.log(event.target.value);
+      setName({ [event.target.name]: event.target.value });
+    }
   };
 
-  submitHandler = event => {
+  const submitHandler = event => {
     event.preventDefault();
-    console.log(this.state);
+
     axios
-      .post("https://ghibli-api-tse.herokuapp.com/people/create", this.state)
+      .post("https://ghibli-api-tse.herokuapp.com/people/create", {
+        name: name.name,
+        gender: gender.gender
+      })
       .then(response => {
         console.log(response);
-        this.props.closeHandler();
-        this.props.afterCreate();
+        props.closeHandler();
+        props.afterCreate();
       })
       .catch(error => {
         console.log(error);
       });
   };
 
-  render() {
-    const { name, gender } = this.state;
-    return (
-      <div className="create-box-container">
-        <h2>Create a character</h2>
-        <form className="create-box-form" onSubmit={this.submitHandler}>
-          <div>
-            <input
-              type="text"
-              name="name"
-              placeholder="Name"
-              value={name}
-              onChange={this.changeHandler}
-            />
-          </div>
-          <div>
-            <input
-              type="text"
-              name="gender"
-              placeholder="Gender"
-              value={gender}
-              onChange={this.changeHandler}
-            />
-          </div>
-          <button className="submit-button" type="submit">
-            Submit
-          </button>
-        </form>
-      </div>
-    );
-  }
+  return (
+    <div className="create-box-container">
+      <h2>Create a character</h2>
+      <form className="create-box-form" onSubmit={submitHandler}>
+        <div>
+          <input
+            type="text"
+            name="name"
+            placeholder="Name"
+            onChange={changeHandler}
+          />
+        </div>
+        <div>
+          <input
+            type="text"
+            name="gender"
+            placeholder="Gender"
+            onChange={changeHandler}
+          />
+        </div>
+        <button className="submit-button" type="submit">
+          Submit
+        </button>
+      </form>
+    </div>
+  );
 }
 
 export default CreateBox;
